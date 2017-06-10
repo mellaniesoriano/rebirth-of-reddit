19ced83cd2454e2fc43e3cf9dd2e537821f6582b
mellaniesoriano <msoriano808@gmail.com>
2017-06-10 10:14:57 -1000

    created div elements w/ DOM, working on getting more data

--- js/app.js
+++ js/app.js
@@ -3,7 +3,22 @@ console.log('sanity check');
 (function() {
   var mainContainer = document.querySelector('#container');
 
-  function createRequest(url) {
+  function makeElems(type, id, content, appendTarget) {
+    var newElement = document.createElement(type);
+    newElement.id = id;
+    newElement.innerHTML = content;
+    appendTarget.appendChild(newElement);
+  }
+
+  makeElems('div', 'titleHeader', 'titleHeader', mainContainer);
+  makeElems('nav', 'navBar', 'navigation', mainContainer);
+  makeElems('div', 'mainPostContainer', 'main post box', mainContainer);
+  makeElems('div', 'postBox', 'indiv post box', mainPostContainer);
+  makeElems('div', 'imgBox', 'image goes here', postBox);
+  makeElems('div', 'infoBox', 'info goes here', postBox);
+  makeElems('footer', 'footer', 'footer goes here', mainContainer);
+
+  function createRequest(url, propertyName) {
     var oReq = new XMLHttpRequest();
     oReq.addEventListener('load', getRedditData);
     oReq.open('GET', url);
@@ -12,16 +27,22 @@ console.log('sanity check');
     function getRedditData() {
       var response = JSON.parse(this.responseText);
       // console.log(response.data);
+      console.log(response.data.children[1].data.preview.images.length);
 
-      for ( var i = 0; i < response.data.children.length; i++ ) {console.log(response.data.children[i].data.preview);
-        var showData = document.createElement('div');
-        showData.id = 'showData';
-        showData.innerHTML = response.data.children[i].data.title;
-        mainContainer.appendChild(showData);
-      }
+      // main data : response.data.children
+      for ( var i = 1; i < response.data.children.length; i++ ) {
+        // console.log(response.data.children[i].data);
+
+        var title = document.createElement('div');
+        title.id = 'showData';
+        title.innerHTML = response.data.children[i].data[propertyName];
+        mainContainer.appendChild(title);
+
+      } // data.children for loop
     } // getRedditData
   } // createRequest
 
-  createRequest('https://www.reddit.com/r/ChildrenFallingOver.json');
+  createRequest('https://www.reddit.com/r/ChildrenFallingOver.json', 'title');
+  // createRequest('https://www.reddit.com/r/ChildrenFallingOver.json', 'thumbnail');
 
 })();
