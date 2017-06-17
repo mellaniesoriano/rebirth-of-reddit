60b90994c138c3f6de59768d5070bc4d873d1efa
mellaniesoriano <msoriano808@gmail.com>
2017-06-17 08:57:40 -1000

    added random descriptions

--- js/app.js
+++ js/app.js
@@ -16,63 +16,64 @@ function getRedditData() {
   for ( var i = 0; i < response.data.children.length; i++ ) {
     var redditData = response.data.children[i].data;
 
-    if ( redditData.url.match(/\.(jpeg|jpg|gif|png)$/) === null ) {
-        continue;
-    }
+    if ( redditData.url.match(/\.(jpeg|jpg|gif|png)$/) !== null ) {
+      var postBox = document.createElement('a');
+      postBox.className = 'postBox';
+      postBox.setAttribute('href', `https://www.reddit.com/${redditData.permalink}`);
+      mainContainer.appendChild(postBox);
 
-    // console.log(`https://www.reddit.com/${redditData.permalink}`)
+      var imgBox = document.createElement('div');
+      imgBox.className = 'imgBox';
+      postBox.appendChild(imgBox);
 
-    var postBox = document.createElement('a');
-    postBox.className = 'postBox';
-    postBox.setAttribute('href', `https://www.reddit.com/${redditData.permalink}`);
-    mainContainer.appendChild(postBox);
+      var images = document.createElement('img');
+      images.className = 'images';
+      images.src = redditData.url;
+      imgBox.appendChild(images);
 
-    var imgBox = document.createElement('div');
-    imgBox.className = 'imgBox';
-    postBox.appendChild(imgBox);
+      var title = document.createElement('a');
+      title.className = 'title';
+      title.innerHTML = redditData.title;
+      title.setAttribute('href', `https://www.reddit.com/${redditData.permalink}`);
+      postBox.appendChild(title);
+      var decodeDate = new Date(redditData.created_utc * 1000);
+      var date = decodeDate.toLocaleDateString();
 
-    var images = document.createElement('img');
-    images.className = 'images';
-    images.src = redditData.url;
-    imgBox.appendChild(images);
+      // var author = document.createElement('a');
+      // author.className = 'author';
+      // author.setAttribute('href', `https://www.reddit.com/user/${redditData.author}`);
 
-    var title = document.createElement('a');
-    title.className = 'title';
-    title.innerHTML = redditData.title;
-    title.setAttribute('href', `https://www.reddit.com/${redditData.permalink}`);
-    postBox.appendChild(title);
-    var decodeDate = new Date(redditData.created_utc * 1000);
-    var date = decodeDate.toLocaleDateString();
+      var postInfo = document.createElement('p');
+      postInfo.className = 'postInfo';
+      postInfo.innerHTML = `by ${redditData.author} &centerdot; ${date} &centerdot; ${redditData.ups} upvotes`;
+      postBox.appendChild(postInfo);
 
-    var postInfo = document.createElement('p');
-    postInfo.className = 'postInfo';
-    postInfo.innerHTML = `by ${redditData.author}, ${date}, ${redditData.ups} upvotes`;
-    postBox.appendChild(postInfo);
+      var descriptionArr = ['Worth a follow on insta.', 'This is what I live for.', 'How far will you fall down this rabbit hole?', 'Please click this link and follow me and like me and tweet me and social media me everything.', 'I always wondered what this would look like upside down.', 'Please send me gold.'];
+      var randomDescriptions = descriptionArr[Math.floor(descriptionArr.length * Math.random())];
 
+      var description = document.createElement('p');
+      description.className = 'description';
+      description.innerHTML = randomDescriptions;
+      postBox.appendChild(description);
     }
   }
+}
 
-  document.querySelector('#randomLink').addEventListener('click', () => {
-    mainContainer.innerHTML = '';
-    var randomArray = ['https://www.reddit.com/r/travel.json','https://www.reddit.com/r/dataisbeautiful.json', 'https://www.reddit.com/r/NatureIsFuckingLit.json', 'https://www.reddit.com/r/wholesomememes.json', 'https://www.reddit.com/r/Art.json'];
-    var randomSubreddit = randomArray[Math.floor(randomArray.length * Math.random())];
-    console.log(randomSubreddit);
-    createRequest(randomSubreddit, getRedditData);
-  });
+document.querySelector('#randomLink').addEventListener('click', () => {
+  mainContainer.innerHTML = '';
+  var randomArray = ['https://www.reddit.com/r/dataisbeautiful.json', 'https://www.reddit.com/r/NatureIsFuckingLit.json', 'https://www.reddit.com/r/wholesomememes.json', 'https://www.reddit.com/r/Art.json'];
+  var randomSubreddit = randomArray[Math.floor(randomArray.length * Math.random())];
+  createRequest(randomSubreddit, getRedditData);
+});
 
-  document.querySelector('#myBoardsLink').addEventListener('click', () => {
-    mainContainer.innerHTML = '';
-    createRequest('https://www.reddit.com/r/StreetArt.json', getRedditData);
-  });
+document.querySelector('#myBoardsLink').addEventListener('click', () => {
+  mainContainer.innerHTML = '';
+  createRequest('https://www.reddit.com/r/travel.json', getRedditData);
+});
 
-  document.querySelector('#getAppLink').addEventListener('click', () => {
-    mainContainer.innerHTML = '';
-    createRequest('https://www.reddit.com/r/Castleporn.json', getRedditData);
-  });
+document.querySelector('#getAppLink').addEventListener('click', () => {
+  mainContainer.innerHTML = '';
+  createRequest('https://www.reddit.com/r/Castleporn.json', getRedditData);
+});
 
-  document.querySelector('#getAppLink').addEventListener('click', () => {
-    mainContainer.innerHTML = '';
-    createRequest('https://www.reddit.com/r/Castleporn.json', getRedditData);
-  });
-
-  createRequest('https://www.reddit.com/r/StreetArt.json', getRedditData);
+createRequest('https://www.reddit.com/r/StreetArt.json', getRedditData);
